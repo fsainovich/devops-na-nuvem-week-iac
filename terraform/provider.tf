53637f154e82aeba48df30d0ab6998b241246a03
Fernando Sainovich <fernando.sainovich@gmail.com>
2024-04-29 10:26:32 -0300

    Updating config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,22 @@
 provider "aws" {
   assume_role {
-    role_arn = "arn:aws:iam::968225077300:role/YoutubeTerraformLiveRole"
+    role_arn = "arn:aws:iam::365176486745:role/fsainovich-eks-assume-role"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "fsainovich-eks-backend"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+    dynamodb_table = "fsainovich-eks-backend"
   }
 }
